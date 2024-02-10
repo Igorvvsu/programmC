ea4d2f9af02d791d529db64a94e2e74d83b55a39
Igorvvsu <159598566+Igorvvsu@users.noreply.github.com>
2024-02-10 15:49:02 +1000

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -20,10 +20,18 @@ int main()
     d = b*b - 4*a*c;
     printf("d = %lf \n",d);
     
-    x1 = (-b+sqrt(d))/(2*a);
-    x2 = (-b-sqrt(d))/(2*a);
+    if (d>0) {
+        x1 = (-b+sqrt(d))/(2*a);
+        x2 = (-b-sqrt(d))/(2*a);
+        printf("x1=%lf x2=%lf \n",x1,x2);
+    }
+    if (d==0) {
+        x1 = (-b/(2*a));
+        printf("x1=%lf \n",x1);
+    }
+    if (d<0) {
+        printf("KORNEY NET");
+    }
     
-    printf("x1=%lf x2=%lf \n",x1,x2);
-
     return 0;
 }
