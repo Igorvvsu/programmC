97ab0c7b6890f5fdefe7c757e0a1b050a4513c61
Igorvvsu <159598566+Igorvvsu@users.noreply.github.com>
2024-02-10 15:41:30 +1000

    Create lab1.c

---
+++ lab1.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a,b,c,d,x1,x2;
+    
+    a=5.1;
+    b=1;
+    c=-2.2;
+    
+    d = b*b - 4*a*c;
+    printf("d = %lf \n",d);
+    
+    x1 = (-b+sqrt(d))/(2*a);
+    x2 = (-b-sqrt(d))/(2*a);
+    
+    printf("x1=%lf x2=%lf \n",x1,x2);
+
+    return 0;
+}
